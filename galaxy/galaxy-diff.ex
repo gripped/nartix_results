349453d250cbee4af4810ab82cef7713565b32aa
gripped <gitkraken@leeharris.me.uk>
2026-02-19 03:51:44 +0000

    2026-02-19 03:51:44+00:00

--- galaxy/galaxy-diff.ex
+++ galaxy/galaxy-diff.ex
@@ -69,6 +69,7 @@ read-edid
 reflector
 sdcv
 signal-desktop
+tealdeer
 terminology
 termshark
 tor
--- galaxy/galaxy-nodiff.ex
+++ galaxy/galaxy-nodiff.ex
@@ -525,7 +525,6 @@ swaync
 swww
 sysbench
 t1lib
-tealdeer
 telegram-desktop
 terminus-font
 tesseract-data
